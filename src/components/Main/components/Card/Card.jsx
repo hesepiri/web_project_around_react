f1da4c2109238c757aa7d6c2a7e5a7d9d5f916b0
hesepiri <hesepiri@gmail.com>
2026-03-22 19:54:49 -0600

    S15 - P1 M4 - Boton Like � en Card.jsx

--- src/components/Main/components/Card/Card.jsx
+++ src/components/Main/components/Card/Card.jsx
@@ -1,15 +1,13 @@
-export default function Card(props) {
-  // Desestructuramos el objeto card que viene desde las props
-  const { name, link } = props.card;
-  const { onCardClick } = props; // Recibimos la función de apertura
+export default function Card({ card, onCardClick }) {
+  const cardLikeButtonClassName = `card__like-button ${card.isLiked ? "card__like-button_is-active" : ""}`;
 
   return (
     <li className="card">
       <img
         className="card__image"
-        src={link}
-        alt={name} // Usa el nombre de la tarjeta para el texto alternativo
-        onClick={() => onCardClick(props.card)} // El trigger para abrir el popup con la tarjeta actual
+        src={card.link}
+        alt={card.name} // Usa el nombre de la tarjeta para el texto alternativo
+        onClick={() => onCardClick(card)} // El trigger para abrir el popup con la tarjeta actual
       />
       <button
         aria-label="Eliminar tarjeta"
@@ -17,11 +15,11 @@ export default function Card(props) {
         type="button"
       />
       <div className="card__description">
-        <h2 className="card__title">{name}</h2>
+        <h2 className="card__title">{card.name}</h2>
         <button
           aria-label="Botón Me gusta"
           type="button"
-          className="card__like-button"
+          className={cardLikeButtonClassName}
         />
       </div>
     </li>
