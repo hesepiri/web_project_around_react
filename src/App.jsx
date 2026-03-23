c072c3424a71bb3e186a5dbdd5b59315bc9035b6
hesepiri <hesepiri@gmail.com>
2026-03-22 19:37:53 -0600

    S15 - P1 M2 - Crear objeto de contexto e utilizar el provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import api from "./utils/api";
+import { use } from "react";
+import CurrentUserContext from "./contexts/CurrentUserContext";
 
 function App() {
+  const [currentUSer, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    api
+      .getUserInfo()
+      .then((userData) => {
+        setCurrentUser(userData);
+      })
+      .catch((err) => {
+        console.error("Error al obtener la información del usuario:", err);
+      });
+  }, []); //Se ejecuta solo una vez al montar el componente
+
   return (
-    <div className="page__content">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <CurrentUserContext.Provider value={currentUSer}>
+      <div className="page__content">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
