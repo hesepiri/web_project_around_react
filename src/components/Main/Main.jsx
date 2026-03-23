fb008d8b31eaffcfd97259c5bcbe9b6516008b70
hesepiri <hesepiri@gmail.com>
2026-03-22 19:29:30 -0600

    S15 - P1 M1 - Crear currentUser

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, use } from "react";
+import React, { useState, useEffect } from "react";
 import api from "../../utils/api";
 import avatar from "../../images/avatar.jpg";
 import Popup from "./components/Popup/Popup";
